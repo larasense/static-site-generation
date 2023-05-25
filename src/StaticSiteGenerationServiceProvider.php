58ce3a7a9a1f78d9332fefec36d6ee29c670edb3
Jose Norberto SANCHEZ <josensanchez@gmail.com>
2023-05-25 17:58:22 -0300

    initial commit

---
+++ src/StaticSiteGenerationServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Larasense\StaticSiteGeneration;
+
+use Illuminate\Support\ServiceProvider;
+
+final class StaticSiteGenerationServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+}
